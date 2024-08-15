b72c814cdb06776d51a300b44193fa0149e742bc
HaiderMalik12 <haidermalik504@gmail.com>
2024-08-15 22:20:41 +0500

    implemented jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { CreateUserDTO } from './create-user-dto';
 import { UsersService } from './users.service';
+import { LoginDTO } from './login-dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +20,19 @@ export class UsersController {
     createUserDTO: CreateUserDTO,
   ) {
     return await this.userService.signup(createUserDTO);
+  }
+
+  @Post('/login')
+  async login(
+    @Body()
+    loginDTO: LoginDTO,
+  ) {
+    return await this.userService.login(loginDTO);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/profile')
+  async getProfile(@Request() req) {
+    return req.user;
   }
 }
